f73470da74b1123a9761618b45e07356c17a0b28
Dom GiGi <38845277+DomGiGi@users.noreply.github.com>
2018-09-13 00:55:39 +0100

    Add files via upload
    Este Esx_Plasticsurgery E Igual A Todos Os Outros Com A Diferenca Que adicionei Blip Que Aparece No Mapa o Original Nao Tem.

---
+++ esx_plasticsurgery/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.Price = 2500
+
+Config.DrawDistance = 100.0
+Config.MarkerSize   = {x = 1.5, y = 1.5, z = 1.0}
+Config.MarkerColor  = {r = 102, g = 102, b = 204}
+Config.MarkerType   = 27
+
+Config.Zones = {}
+
+Config.Shops = {
+  {x = -450.37,  y = -340.08,  z = 34.5},
+}
+
+for i=1, #Config.Shops, 1 do
+	Config.Zones['Shop_' .. i] = {
+	 	Pos   = Config.Shops[i],
+	 	Size  = Config.MarkerSize,
+	 	Color = Config.MarkerColor,
+	 	Type  = Config.MarkerType
+  }
+
+end
